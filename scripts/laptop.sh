6ff50232a591e8f51775113083d92619e8f5a9a2
Michelle Liu <mmliu@andrew.cmu.edu>
2026-03-22 16:08:28 -0400

    moved mobility to laptop

--- scripts/laptop.sh
+++ scripts/laptop.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+SIM=false
+
+for arg in "$@"; do
+    case $arg in
+        --sim)
+        SIM=true
+        shift
+        ;;
+        *)
+        ;;
+    esac
+done
+
+if [ "$SIM" = true ]; then
+    echo -e "\nLaunching laptop tmux sessions (simulation)...\n"
+else
+    echo -e "\n:aunching laptop tmux sessions (hardware)...\n"
+fi
+
 colcon build --packages-select fb_bringup --symlink-install
 colcon build --packages-select fb_planning --symlink-install
 colcon build --packages-select fb_vision --symlink-install
@@ -11,12 +30,31 @@ ros2 pkg list | grep fb_
 ######
 
 ./src/fb_bringup/scripts/foxglove_bridge.sh
-./src/fb_bringup/scripts/cameras_laptop.sh
+# ./src/fb_bringup/scripts/cameras_laptop.sh
 
 ./src/fb_planning/scripts/central_planner.sh
 ./src/fb_planning/scripts/path_planner.sh
 
 ./src/fb_vision/scripts/vision.sh
+
+
+if [ "$SIM" = true ]; then
+    ./src/fb_gazebo/scripts/gazebo.sh
+    ./src/fb_mobility/scripts/teleop.sh
+
+    ./src/fb_manipulation/scripts/manipulation_sim.sh
+
+    sleep 1
+    tmux attach-session -t manipulation_sim
+else
+    ./src/fb_mobility/scripts/diff_drive.sh
+    ./src/fb_mobility/scripts/teleop.sh
+
+    ./src/fb_manipulation/scripts/manipulation.sh
+
+    sleep 1
+    tmux attach-session -t manipulation
+fi
 
 sleep 1
 tmux attach-session -t central_planner
--- scripts/robot.sh
+++ scripts/robot.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-SIM=false
-
-for arg in "$@"; do
-    case $arg in
-        --sim)
-        SIM=true
-        shift
-        ;;
-        *)
-        ;;
-    esac
-done
-
-if [ "$SIM" = true ]; then
-    echo -e "\nLaunching robot tmux sessions (simulation)...\n"
-else
-    echo -e "\n:aunching robot tmux sessions (hardware)...\n"
-fi
-
-
 colcon build --packages-select fb_bringup --symlink-install
 colcon build --packages-select fb_gazebo --symlink-install
 colcon build --packages-select fb_mobility --symlink-install
@@ -33,25 +13,7 @@ tmux kill-server
 
 ######
 
-# ./fb_bringup/scripts/cameras_robot.sh # UNUSED
+./fb_bringup/scripts/cameras_robot.sh
+./src/fb_bringup/scripts/arduino_bridge.sh
 
-if [ "$SIM" = true ]; then
-    ./src/fb_gazebo/scripts/gazebo.sh
-    ./src/fb_mobility/scripts/teleop.sh
-
-    ./src/fb_manipulation/scripts/manipulation_sim.sh
-
-    sleep 1
-    tmux attach-session -t manipulation_sim
-else
-    ./src/fb_bringup/scripts/arduino_bridge.sh
-
-    ./src/fb_mobility/scripts/diff_drive.sh
-    ./src/fb_mobility/scripts/teleop.sh
-
-    ./src/fb_manipulation/scripts/manipulation.sh
-
-    sleep 1
-    tmux attach-session -t manipulation
-fi
 
